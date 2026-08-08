7fb924d2df2f278f4700bdd76793324d6361a817
miltonHenschel <nketchoguemilton@gmail.com>
2026-08-08 08:43:41 +0100

    added background image for mobile version

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-window.process = window.process || { env: { NODE_ENV: 'production' } };
-
-// Tab functionality
-const tablinks = document.getElementsByClassName("tab-links");const tabcontents = document.getElementsByClassName("tab-contents");
-for (const tablink of tablinks){
-  tablink.addEventListener("click", function(opentab) {
-    for (const tabcontent of tabcontents) {
-      tabcontent.classList.remove("active-tab");
-    }
-    for (const tablink of tablinks) {
-      tablink.classList.remove("active-link");
-    }
-    opentab.currentTarget.classList.add("active-link");
-    const tabName = opentab.currentTarget.dataset.target;
-    document.getElementById(tabName).classList.add(`active-tab`);
-  });
-}
+window.process=window.process||{env:{NODE_ENV:"production"}};const tablinks=document.getElementsByClassName("tab-links"),tabcontents=document.getElementsByClassName("tab-contents");for(const t of tablinks)t.addEventListener("click",function(t){for(const t of tabcontents)t.classList.remove("active-tab");for(const t of tablinks)t.classList.remove("active-link");t.currentTarget.classList.add("active-link");const e=t.currentTarget.dataset.target;document.getElementById(e).classList.add("active-tab")});
